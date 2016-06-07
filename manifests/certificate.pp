26a734511e2040f1ce74789db9c1fd465166c256
matthias <matthias@icontroller.be>
2016-06-07 14:47:54 +0200

    Added automatic pem support

--- manifests/certificate.pp
+++ manifests/certificate.pp
@@ -1,27 +1,39 @@
 define ssl_cert::certificate(
-	$ensure,
+    $ensure,
 ) {
-	include ssl_cert
+    include ssl_cert
 
-	$base_dir = $ssl_cert::base_dir
-	$certs = $ssl_cert::certs
+    $base_dir = $ssl_cert::base_dir
+    $certs = $ssl_cert::certs
 
-	$defaults =  {
-		owner => 'root',
-		group => 'root',
-		ensure => 'present',
-		mode => '0640',
-		requirements => [],
-	}
+    if ! has_key($certs, $name) {
+        fail("Could not find definition for ssl certificate $name")
+    }
 
-	$options = merge($defaults, $certs[$name], { ensure => $ensure })
+    $defaults =  {
+        owner => 'root',
+        group => 'root',
+        ensure => 'present',
+        mode => '0640',
+        requirements => [],
+    }
 
-	if $ensure == 'present' {
-		$options['requirements'].each|$requirement| {
-			ensure_resource('ssl_cert::certificate', $requirement, { ensure => $ensure })
-		}
-	}
+    $options = merge(
+        $defaults,
+        $certs[$name],
+        { ensure => $ensure },
+        has_key($certs[$name], 'concat') ? {
+            true => { content => join($certs[$name]['concat'].map |$cname| { $certs[$cname]['content'] }, "\n") },
+            default => {}
+        }
+    )
 
-	$file_options = delete($options, 'requirements')
-	ensure_resource('file', "${base_dir}/${title}", $file_options)
+    if $ensure == 'present' {
+        $options['requirements'].each|$requirement| {
+            ensure_resource('ssl_cert::certificate', $requirement, { ensure => $ensure })
+        }
+    }
+
+    $file_options = delete($options, ['requirements', 'concat'])
+    ensure_resource('file', "${base_dir}/${title}", $file_options)
 }
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,33 +1,33 @@
 class ssl_cert(
-	$enable = [],
-	$disable = [],
-	$dirs = ['/etc/ssl/ca', '/etc/ssl/keys'],
-	$base_dir = '/etc/ssl',
-	$notify_services = ['httpd']
+    $enable = [],
+    $disable = [],
+    $dirs = ['/etc/ssl/ca', '/etc/ssl/keys'],
+    $base_dir = '/etc/ssl',
+    $notify_services = ['httpd', 'haproxy']
 ) {
-	$certs = hiera_hash('ssl_cert::certs', {})
-	$append_enable = hiera_array('ssl_cert::append_enable', [])
-	$append_disable = hiera_array('ssl_cert::append_disable', [])
+    $certs = hiera_hash('ssl_cert::certs', {})
+    $append_enable = hiera_array('ssl_cert::append_enable', [])
+    $append_disable = hiera_array('ssl_cert::append_disable', [])
 
-	$all = {
-		present => union($enable, $append_enable),
-		absent => union($disable, $append_disable),
-	}
+    $all = {
+        present => union($enable, $append_enable),
+        absent => union($disable, $append_disable),
+    }
 
-	file { $dirs:
-		ensure => directory,
-		owner => root,
-		group => root,
-	}
+    file { $dirs:
+        ensure => directory,
+        owner => root,
+        group => root,
+    }
 
-	$all.each|$ensure, $cert_names| {
-		$cert_names.each|$name| {
-			ensure_resource('ssl_cert::certificate', $name,  { ensure => $ensure })
-		}
-	}
+    $all.each|$ensure, $cert_names| {
+        $cert_names.each|$name| {
+            ensure_resource('ssl_cert::certificate', $name,  { ensure => $ensure })
+        }
+    }
 
-	File[$dirs] -> Certificate<| |>
-	$notify_services.each|$service| {
-		Certificate<| |> ~> Service<| title == $service |>
-	}
+    File[$dirs] -> Certificate<| |>
+    $notify_services.each|$service| {
+        Certificate<| |> ~> Service<| title == $service |>
+    }
 }
